48ff0829b36020959cca77f3c4778cce2e89ee25
Nurul Abser <abser.it@gmail.com>
2015-05-21 16:16:43 +0800

    first test

---
+++ test/projectTest.php
@@ -0,0 +1,11 @@
+<?php
+class ProjectTest extends PHPUnit_Framework_TestCase {
+
+	public function testInsertProject() {
+		$a = array(1,2,3);
+		$this->assertEquals(3, count($a));
+
+		array_push($a, 4);
+		$this->assertEquals(4, count(4a));
+	}
+}
