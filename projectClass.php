6b5511572bf7e84bd96a15d6f157664a5698bae3
Nurul Abser <abser.it@gmail.com>
2015-05-21 14:53:01 +0800

    first commit

---
+++ projectClass.php
@@ -0,0 +1,15 @@
+<?php
+//Project base class
+class projectClass{
+
+	function __construct(){
+
+	}
+
+	public class createProject(){
+		$name = $_POST['projectName'];
+		$start = $_POST['start'];
+		$end = $_POST['end'];
+		$budget = $_POST['budget'];
+	}
+}
